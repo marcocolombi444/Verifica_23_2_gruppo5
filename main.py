f7d09083f3e3bc452a0b4a04ddb4b1fe377bee58
marcocolombi444 <119593538+marcocolombi444@users.noreply.github.com>
2023-02-23 13:07:29 +0100

    main code
    stefano mancante

---
+++ main.py
@@ -0,0 +1,14 @@
+import castellani_lorenzo_Gruppo_5
+import cavalieri_stefano_5
+
+def saluto_colombi_marco_5():
+    print("Buongiorno, Marco Frollombni")
+
+def main():
+    castellani_lorenzo_Gruppo_5.saluto_castellani_lorenzo_Gruppo_5()
+    cavalieri_stefano_5.saluto_cavalieri_stefano_5()
+    saluto_colombi_marco_5()
+
+
+
+main()
