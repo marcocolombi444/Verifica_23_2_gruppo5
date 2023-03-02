c1f8bb8e5b839213010211972e1e05018d48d695
marcocolombi444 <119593538+marcocolombi444@users.noreply.github.com>
2023-03-02 12:24:42 +0100

    correzione nome dei file da importare
    negli import i file non erano stati scritti nella maniera corretta

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import castellani_lorenzo_Gruppo_5
+import castellani_lorenzo_5
 import cavalieri_stefano_5
 
 def saluto_colombi_marco_5():
     print("Buongiorno, Marco Frollombni")
 
 def main():
-    castellani_lorenzo_Gruppo_5.saluto_castellani_lorenzo_Gruppo_5()
-    cavalieri_stefano_5.saluto_cavalieri_stefano_5()
+    castellani_lorenzo_5.saluto_castellani_lorenzo_Gruppo_5()
+    cavalieri_stefano_5.saluto_cavalieri_stefano_Gruppo_5()
     saluto_colombi_marco_5()
 
 
